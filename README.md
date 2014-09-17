7726316876db62127edaac0981c0e817adb1e095
Andrew Clissold <andrewclissold@gmail.com>
2014-09-17 00:28:24 -0400

    Create noise.go

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ pixelbox
 A sandbox for image processing ideas.
 
 `go run <file>.go`
+
+![noise](https://raw.githubusercontent.com/aclissold/pixelbox/master/noise.png)
---
+++ noise.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var size int
+var img *image.RGBA
+var r *rand.Rand
+
+func main() {
+	setup()
+	draw()
+	write()
+}
+
+func setup() {
+	size = 1024
+	img = image.NewRGBA(image.Rect(0, 0, size, size))
+
+	t := time.Now().UnixNano()
+	s := rand.NewSource(t)
+	r = rand.New(s)
+}
+
+func draw() {
+	for x := 0; x < size; x++ {
+		for y := 0; y < size; y++ {
+			gray := color.Gray16{uint16(r.Intn(65536))}
+			img.Set(x, y, gray)
+		}
+	}
+}
+
+func write() {
+	file, err := os.Create("noise.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = png.Encode(file, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
